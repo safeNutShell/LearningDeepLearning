91c1a3aecbeb9d0b11773e5e5b56d06bfbb848e7
SK.Wang <46984866+safeNutShell@users.noreply.github.com>
2021-10-17 23:27:53 +0800

    Create Optimize&Save.py

---
+++ ABC/Optimize&Save.py
@@ -0,0 +1,104 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+
+train_dataloader = DataLoader(training_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = NeuralNetwork().to(device)
+
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    # calculate prediction and loss
+    for batch, (X, y) in enumerate(dataloader):
+        X = X.cuda()
+        y = y.cuda()
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # backpropagation on 64 batch_size
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:5d}]")
+
+
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            X = X.cuda()
+            y = y.cuda()
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+# set hyper parameters
+learning_rate = 1e-3
+epochs = 10
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+for t in range(epochs):
+    print(f"Epoch {t + 1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model,  loss_fn)
+
+# save model shape
+# torch.save(model, 'model_shape.pth')
+# save model parameters
+# torch.save(model.state_dict(), 'model_weights.pth')
+# load model
+# model_load = torch.load('model.pth')
+# load model parameters
+# model_load.load_state_dict(torch.load('model_weights.pth'))
